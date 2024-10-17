ad46f44a5f0d16e0b19f7d362aed9d9eb413c57b
JonasGodoy600 <59368280+JonasGodoy600@users.noreply.github.com>
2024-10-17 18:11:24 -0300

    Add files via upload

--- colecoes/VetorDinamico.java
+++ colecoes/VetorDinamico.java
@@ -1,11 +1,77 @@
 public class VetorDinamico {
     private int qtde;
     private int cap;
-    private int [] elementos;
-
+    private int [] elementos; //refencia para vetor referente de inteiro
+    private static final int CAPACIDADE_MINIMA = 4; //variavel de classe pelo static e inalteravel pelo final
+    
     public void adicionar(int e){
-        elementos[qtde] = e;
-        qtde++;
+        if(estaCheio()){    //verificar se o vetor esta cheio se estiver chamado para aux com o dobro de elementos 
+            redimencionar(2);
+        }
+        elementos[qtde++] = e;
+        
+    }
+    private void redimencionar(double fator){
+        int [] aux;
+        aux = new int [(int) (cap * fator)];
+        for(var i = 0; i < qtde; i++){
+            aux[i] = elementos[i];
+        }
+        cap = (int) (cap * fator);
+        elementos = aux;
+
+    }
+
+    private void aumentarCapacidade(){
+            int [] aux;
+            aux = new int [cap*2];
+            for(var i = 0; i < qtde; i++){
+                aux[i] = elementos[i];
+            }
+            cap = cap * 2;
+            elementos = aux;
+
+    }
+
+    private void reduzirCapacidade(){
+        //vetor auxliar
+        //copiar todo mundo para o aux
+        if(estaUmQuartoCheio()){
+            int [] aux;
+            aux = new int[cap/2];
+            for (int i =0; i < qtde; i++){
+                aux[i] = elementos[i];
+            }
+            cap = cap / 2;
+            elementos = aux;
+        }
+    }
+
+    public void remover(){
+        // se não esta vazio remova
+        if (!estaVazio()){
+            qtde--;
+            
+        }
+        if (estaUmQuartoCheio() && cap > CAPACIDADE_MINIMA){
+            redimencionar(0.5);
+        }
+    }
+
+    public boolean estaVazio(){
+        return qtde == 0;
+    }
+
+    private boolean estaUmQuartoCheio(){
+        return qtde <= cap * 4;
+    }
+
+    public boolean estaCheio(){
+        return qtde == cap;
+        // return qtde == cap ? true : false;
+        // if(qtde == cap)
+        // return true;
+        // return false;
     }
 
     public VetorDinamico(int cap){
